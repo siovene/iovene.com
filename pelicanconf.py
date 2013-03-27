dc38857c4c1d7f5508907e5173238d1c476d2489
Salvatore Iovene <salvatore@iovene.com>
2013-03-27 21:07:25 +0200

    Add a bunch of settings.

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,14 +10,22 @@ SITEDESCR = u'Hi, I am a software engineer and astrophotographer living in '\
 'Espoo, Finland, and working for the Intel Open Source Technology Centre.'
 
 TIMEZONE = 'Europe/Paris'
-
 DEFAULT_LANG = u'en'
-
-# Blogroll
-LINKS =  ()
-
-# Social widget
-SOCIAL = ()
 
 DEFAULT_PAGINATION = 10
 THEME = 'iovene.com-theme-alpha'
+
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
+
+TAG_URL = 'tag/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+
+ARCHIVES_URL = 'archives/'
+ARCHIVES_SAVE_AS = ARCHIVES_URL + 'index.html'
